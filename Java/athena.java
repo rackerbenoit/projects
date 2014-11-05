8fca508a404399b3b76a6525ab26fc5d9cad4323
Paul Benoit <paul.benoit36@gmail.com>
2014-11-04 19:57:44 -0600

    Cleaned up

--- Java/athena.java
+++ Java/athena.java
@@ -1,15 +1,16 @@
-//Build the deck 1 -13
-//Shuffle the deck
-//Play through the game
-//print the card that ius drawn and the state of the oard # of discards
-//Won or lost
-//10 slots
-//First goal: Get game to play
-//Second: optimize
+/* Build the deck numbered 1 -13
+ * Shuffle the deck
+ * Play through the game
+ * print the card that is drawn and the state of the board along with
+ * the # of discards
+ * Display Won or lost
+ * 10 slots
+ * First goal: Get game to play
+ * Second: optimize
 
-// Initialze array of 52
-// Shuffle the array
-// Discard 3 cards always
+ * Initialze array of 52
+ * Shuffle the array
+ * Discard 3 cards always for naive method */
 
 
 import java.util.*;
@@ -57,16 +58,11 @@ class athena {
     }
     System.out.println("Card # in slot: " + slots.get(i));
     }
-
-
-
     //Once everything is initialzed to the correct value, I will need to shuffle the array
     //Once everything is suffled, I will defined the 10 slots.
     //Draw a card: Hardcode which cards to automaticallly discard.
     //The cards that will not be discarded need to be assigned a particular slot
     //If less than 6 cards have been discraded and all slots are full, you win the game
     //Otherwise, you lose
-
-
   }
 }
